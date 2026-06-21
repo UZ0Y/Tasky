9491b19e7b3ff36cb9d5411c0e81e828d2b3b032
Yaamor Mokdad <ahmadlink9393@example.com>
2026-06-22 00:48:26 +0300

    ready

--- src/bot/main.py
+++ src/bot/main.py
@@ -2,7 +2,7 @@ import discord
 from dotenv import load_dotenv
 import aiohttp, asyncio, os, logging
 
-from shared import database
+from src.shared import database
 
 load_dotenv()
 handler = logging.FileHandler(filename="Discord.log", mode="w", encoding="utf-8")
