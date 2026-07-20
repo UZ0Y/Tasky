9981bde4b346e9b8bc09c31480a08fd9343b45a4
Iyad <Iyaddawoud1@gmail.com>
2026-07-20 14:56:41 +0300

    added add_message_history function \nWrapped db in class

--- src/shared/database.py
+++ src/shared/database.py
@@ -1,26 +1,47 @@
 import aiosqlite
+from src.shared.config import DB_PATH
 
-from .config import DB_PATH
+class Database:
+    def __init__(self, db_path=DB_PATH):
+        self.db_path = db_path
 
-async def initialize_db():
-    """Initializes the database and creates the necessary tables."""
-    async with aiosqlite.connect(DB_NAME) as db:
-        await db.execute("""
-            CREATE TABLE IF NOT EXISTS TASKS (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                author_id INTEGER,
-                head TEXT UNIQUE,
-                body TEXT,
-                timestamp TEXT
+    async def initialize_db(self):
+        """Initializes the database and creates the necessary tables."""
+        async with aiosqlite.connect(self.db_path) as db:
+            await db.execute("""
+                CREATE TABLE IF NOT EXISTS TASKS (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    author_id INTEGER,
+                    head TEXT UNIQUE,
+                    body TEXT,
+                    timestamp TEXT
+                )
+            """)
+            await db.execute("""
+                CREATE TABLE IF NOT EXISTS MESSAGE_HISTORY (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    author_id INTEGER,
+                    channel_id INTEGER,
+                    content TEXT,
+                    timestamp TEXT
+                )
+            """)
+            await db.commit()
+
+    async def add_task(self, author_id, head, body, iso_time_stamp):
+        """Inserts a new task into the database."""
+        async with aiosqlite.connect(self.db_path) as db:
+            await db.execute(
+                "INSERT INTO TASKS (author_id, head, body, timestamp) VALUES (?, ?, ?, ?)",
+                (author_id, head, body, iso_time_stamp)
             )
-        """)
-        await db.commit()
+            await db.commit()
 
-async def add_task(author_id, head, body, iso_time_stamp):
-    """Inserts a new task into the database."""
-    async with aiosqlite.connect(DB_NAME) as db:
-        await db.execute(
-            "INSERT INTO TASKS (author_id, head, body, timestamp) VALUES (?, ?, ?, ?)",
-            (author_id, head, body, iso_time_stamp)
-        )
-        await db.commit()
+    async def add_message_history(self, author_id, channel_id, content, iso_time_stamp):
+        """Inserts a new message log into the database."""
+        async with aiosqlite.connect(self.db_path) as db:
+            await db.execute(
+                "INSERT INTO MESSAGE_HISTORY (author_id, channel_id, content, timestamp) VALUES (?, ?, ?, ?)",
+                (author_id, channel_id, content, iso_time_stamp)
+            )
+            await db.commit()
